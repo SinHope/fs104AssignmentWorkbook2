0b2069671035c220d0a70e33de9609485d81bd8c
Nasmer Fontanilla <nasmerfontanilla@gmail.com>
2023-12-13 00:22:58 +0800

    Updated index.js for EJS configuration 2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files from the 'assets' directory
 app.use(express.static('assets'));
